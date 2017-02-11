66923eaa56fd33acc38e4f2c5c63e95b758ce1da
Neves <HelioNeves@users.noreply.github.com>
2017-02-11 05:29:31 -0200

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -1,16 +1,27 @@
 $(document).ready(function(){
-  $("img:not(#first)").mouseenter(function(){
+  $("img:not(#first, #botao)").mouseenter(function(){
     $(this).animate({
         width: '-=10px',
         height: '-=10px'
       });
   });
 
-  $("img:not(#first)").mouseleave(function(){
+  $("img:not(#first, #botao)").mouseleave(function(){
     $(this).animate({
         width: '+=10px',
         height: '+=10px'
       });
   });
+
+  $("#botao").on({
+ "mouseover" : function() {
+    this.src = 'img/button.png';
+  },
+  "mouseout" : function() {
+    this.src='img/buttonclick.png';
+  }
+});
+
+  $('[data-toggle="tooltip"]').tooltip();
 
 });
